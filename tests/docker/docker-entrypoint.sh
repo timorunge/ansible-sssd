e896d3fd1fcddf7465b14dd29a96ef7826fb1802
Timo Runge <me@timorunge.com>
2018-09-04 16:28:15 +0200

    * Making the docker-entrypoint * Moving configuration for the docker_test_runner to config file

--- tests/docker/docker-entrypoint.sh
+++ tests/docker/docker-entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 test -z ${sssd_from_sources} && echo "Missing environment variable: sssd_from_sources" && exit 1
-(test -z ${sssd_version} && test "${sssd_from_sources}" = "true") && echo "Missing environment variable: sssd_version" && exit 1
+(test "${sssd_from_sources}" = "true" && test -z ${sssd_version}) && echo "Missing environment variable: sssd_version" && exit 1
 
-printf "[defaults]\nroles_path=/etc/ansible/roles\ngather_timeout=60" > ansible.cfg
+printf "[defaults]\nroles_path=/etc/ansible/roles\n" > ansible.cfg
+
 ansible-lint /ansible/test.yml
 ansible-lint /etc/ansible/roles/${ansible_role}/tasks/main.yml
 
@@ -35,6 +36,6 @@ ansible-playbook /ansible/test.yml \
 if [ "true" = "${sssd_from_sources}" ] ; then
   real_sssd_version=$(sssd --version 2>&1)
   test "${real_sssd_version}" = "${sssd_version}" && \
-    (echo 'SSSD version test: pass' && exit 0) || \
-    (echo 'SSSD version test: fail' && exit 1)
+    (echo "SSSD version test: pass" && exit 0) || \
+    (echo "SSSD version test: fail" && exit 1)
 fi
