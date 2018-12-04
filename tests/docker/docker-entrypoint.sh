61a00dda9869063fc1f43ca31b836de7083a6b3a
Timo Runge <me@timorunge.com>
2018-12-04 17:58:57 +0100

    * Adding package installation retry logic. * https://github.com/ansible/galaxy-lint-rules is depricated; falling back to default rules.

--- tests/docker/docker-entrypoint.sh
+++ tests/docker/docker-entrypoint.sh
@@ -6,17 +6,9 @@ test -z ${sssd_from_sources} && echo "Missing environment variable: sssd_from_so
 
 printf "[defaults]\nroles_path=/etc/ansible/roles\n" > /ansible/ansible.cfg
 
-if [ ! -f /etc/ansible/lint.zip ]; then
-  wget https://github.com/ansible/galaxy-lint-rules/archive/master.zip -O \
-  /etc/ansible/lint.zip
-  unzip /etc/ansible/lint.zip -d /etc/ansible/lint
-fi
-
-ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint -r \
-  /etc/ansible/lint/galaxy-lint-rules-master/rules \
+ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint \
   /etc/ansible/roles/${ansible_role}
-ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint -r \
-  /etc/ansible/lint/galaxy-lint-rules-master/rules \
+ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint \
   /ansible/test.yml
 
 ansible-playbook /ansible/test.yml \
